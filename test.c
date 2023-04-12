5c2c8a17b4d733d481e146117aa8c24c58bdc9fb
hongjoo <frogshj99@naver.com>
2023-04-12 11:58:06 +0900

    Delete all contents

--- test.c
+++ test.c
@@ -1,17 +1,6 @@
 #include <stdio.h>
 int main() {
-    printf("hello, world!\n");
-
-    printf("git text\n\n");
-    
-    printf("git test\n");
-    printf("git test\n");
-    printf("git test\n");
-    printf("git test\n");
-    printf("git test\n");
-    printf("git test\n");
-    
-    printf("testing 새로운 한글 폰트\n");
+    printf("안녕, 세상!\n");
 
     return 0;
 }
